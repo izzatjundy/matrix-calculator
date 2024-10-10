e890aa83748cf0a696b8335aa28e9035f90d8087
DiyahSusan <diyahnugrahani04@gmail.com>
2024-10-10 20:03:58 +0700

    fix detOBE

--- src/ADTMatrix/Determinan.java
+++ src/ADTMatrix/Determinan.java
@@ -53,69 +53,17 @@ public class Determinan {
 
     //mencari determinan dengan OBE
     public static double detOBE(Matrix m){
-        double det = 1;
-        int i, j, k, l;
-        double x, y;
-        int row = m.getRowLength();
-        int col = m.getColLength();
-        
-        //apakah matrix persegi
-        if(m.isSquare()){
-            if (row==1 || col==1){
-                return m.matrix[0][0];
-            }
-            else{
-                for (i=0; i<row; i++){
-                //jika elemen diagonal 0, cari baris di bawahnya untuk ditukar
-                    if (m.matrix[i][i] == 0){
-                        j = i+1;
-                        while(j<row && m.matrix[j][i] == 0){
-                            j++;
-                        }
-                        //jika  sudah ketemu bukan 0, lakukan pertukaran baris
-                        if (j<row){
-                            //tukar baris
-                            for (k=0; k<row; k++){
-                                double temp = m.matrix[i][k];
-                                m.matrix[i][k] = m.matrix[j][k];
-                                m.matrix[j][k] = temp;
-                            }
-                            det *= -1; //dikali -1 karena ditukar
-                        }
-                        else{
-                            det = 0;
-                            break;
-                        }
-                    }
+        //matriks di OBE kan dulu menggunakan eliminasiGauss
+        m.gaussElimination();
 
-                    //normalisasi baris untuk menjadikan elemen diagonal = 1
-                    x = m.matrix[i][i];
-                    det *= x;
-                    if (x != 0){
-                        for (j=0; j<row; j++){
-                            m.matrix[i][j] /= x;
-                        }
-                    }
-
-                    //eliminasi kolom di bawah elemen diagonal
-                    for (k=i+1; k<row; k++){
-                        y = m.matrix[k][i];
-                        for (l=0; l<row; l++){
-                            m.matrix[k][l] -= m.matrix[i][l] * y;
-                        }
-                    return det;
-                        
-                    }
-
-                }
-            }
-        
+        //menghitung determinan berdasarkan perkalian diagonal
+        double determinan = 1;
+        for (int i=0; i<m.getRowLength(); i++){
+            determinan *= m.matrix[i][i]; //mengalikan elemen diagonal
         }
-        else{
-            return m.MARK;
-        }
-        return 0;
-        
+
+        return determinan;
     }
+
 }
 
